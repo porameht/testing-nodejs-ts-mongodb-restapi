0380872c2231fa9df915298cbc72f239f5a3b6ba
fr4nk <89307294+porameht@users.noreply.github.com>
2022-08-16 15:45:13 +0700

    product api result is passing

--- readme.md
+++ readme.md
@@ -5,15 +5,19 @@
 - Testing from the controller to the service
 
 1. install dependencies `yarn add supertest jest ts-jest @types/jest @types/supertest mongo-memory-server -D`
+
 2. create config file `yarn ts-jest config:init`
 
    - add test match verbose and forceExit `testMatch: ["**/**/*.test.ts"], verbose: true, forceExit: true,`
 
 3. come in to `package.json` for add `"script" : {"test":"jest"}` can use run test `yarn test` or set auto test by `yarn test --watchAll` or `yarn test --detectOpenHandles`
+
 4. let's go to folder `src` then ceate folder `__test__` then create file
 
    - create file test `product.test.ts` and implement test case of route `"/api/products/:productId"`
+
    - import `supertest from 'supertest'`
+
    - import `app from app.ts` before go to file `app.ts` for export `app`
 
 5. let's go to `utils` for ceate file `server.ts` then create function `createServer`
@@ -23,5 +27,15 @@
    - create mongo server by `beforeAll` and `afterAll`
 
 7. define `productPayload` and define `userId` for test status 200 `should return a 200 status and the product`
+
    - let's go to `product.model.ts` and add `productId: string;` to interface `ProductDocument`
+
    - let's go to `product.service.model` and add `"productId"` to `DocumentDefinition` of function `createProduct`
+
+8. create describe `"create product route"`
+
+   - create test case `given the user is not logged in` middlewate is require `requireUser`
+
+   - create test case `given the user is logged in` define `jwt` then create user payload `userPayload`
+     - test first that fail by `expect(body).toEqual({})` then copy fail result pass to `.toEqual({})`
+     - change key that from fail to `expect.any(String)`
--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -3,9 +3,9 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import createServer from "../utils/server";
 import mongoose from "mongoose";
 import { createProduct } from "../service/product.service";
+import { signJwt } from "../utils/jwt.utils";
 
 const app = createServer();
-
 const userId = new mongoose.Types.ObjectId().toString();
 
 export const productPayload = {
@@ -17,10 +17,15 @@ export const productPayload = {
   image: "https://i.imgur.com/QlRphfQ.jpg",
 };
 
+export const userPayload = {
+  _id: userId,
+  email: "frank.d@example.com",
+  name: "frank.k",
+};
+
 describe("product", () => {
   beforeAll(async () => {
     const mongoServer = await MongoMemoryServer.create();
-
     await mongoose.connect(mongoServer.getUri());
   });
 
@@ -58,9 +63,37 @@ describe("product", () => {
     describe("given the user is not logged in", () => {
       it("should return a 403", async () => {
         const { statusCode } = await supertest(app).post("/api/products");
-
         expect(statusCode).toBe(403);
       });
     });
+
+    describe("given the user is logged in", () => {
+      it("should return a 200 and create the product", async () => {
+        const jwt = signJwt(userPayload);
+
+        const { statusCode, body } = await supertest(app)
+          .post("/api/products")
+          .set("Authorization", `Bearer ${jwt}`)
+          .send(productPayload);
+
+        expect(statusCode).toBe(200);
+
+        expect(body).toEqual({
+          __v: 0,
+          _id: expect.any(String),
+          createdAt: expect.any(String),
+          description:
+            "Designed for first-time DSLR owners who want impressive results straight out of the box, capture those magic moments no matter your level with the EOS 1500D. With easy to use automatic shooting modes, large 24.1 MP sensor, Canon Camera Connect app integration and built-in feature guide, EOS 1500D is always ready to go.",
+          image: "https://i.imgur.com/QlRphfQ.jpg",
+          price: 879.99,
+          productId: expect.any(String),
+          title: "Canon EOS 1500D DSLR Camera with 18-55mm Lens",
+          updatedAt: expect.any(String),
+          user: expect.any(String),
+        });
+      });
+    });
   });
+
+  
 });
